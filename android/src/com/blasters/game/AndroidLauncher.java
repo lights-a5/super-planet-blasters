cb550468b2c3d29f9a446505b2c186f575f09685
OHNOSROFLCOPTER <srymnir@gmail.com>
2016-10-31 15:25:26 +0000

    Initial commit

---
+++ android/src/com/blasters/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.blasters.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.blasters.game.SuperPlanetBlasters;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SuperPlanetBlasters(), config);
+	}
+}
