f3163e31cafb584518eb29fa9af49cd69f01532b
Adam Gehring <gehringadam@byui.edu>
2016-11-03 15:02:15 -0600

    Hammering on this menu

--- core/src/com/blasters/game/screens/MenuScreen.java
+++ core/src/com/blasters/game/screens/MenuScreen.java
@@ -4,8 +4,67 @@ package com.blasters.game.screens;
  * Created by Adam on 11/1/2016.
  */
 
-public class MenuScreen {
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.blasters.game.SuperPlanetBlasters;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 
-    //to put menu here
+public class MenuScreen implements Screen{
+    
+
+    private OrthographicCamera camera;
+    private SpriteBatch sb;
+    public Actor button;
+    public MenuScreen(SuperPlanetBlasters game) {
+
+        super();
+
+    }
+
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        super.resize( width, height );
+        final float buttonX = ( width - BUTTON_WIDTH ) / 2;
+        float currentY = 280f;
+
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
 
 }
