ae3231534fcc6261e6f857d93c938853b755199b
Adam Gehring <gehringadam@byui.edu>
2016-12-05 10:15:50 -0700

    The Ist boss works great the yellow powerup is OP

--- core/src/com/blasters/game/sprites/DeceptaBoss.java
+++ core/src/com/blasters/game/sprites/DeceptaBoss.java
@@ -1,5 +1,6 @@
 package com.blasters.game.sprites;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.blasters.game.gameworld.GameWorld;
@@ -10,30 +11,58 @@ import com.blasters.game.gameworld.GameWorld;
 
 public class DeceptaBoss extends Fighter {
 
+    boolean moveRight;
+
     public DeceptaBoss(GameWorld world) {
         super(world);
     }
 
-    Sprite bossSprite;
-
     protected void defineFighter() {
+        moveRight = false;
         value = 100;
-        health  = 120;
+        health  = 600;
         TextureRegion fighter = world.getAtlas().findRegion("deceptaBoss"); //updated to reflect new atlas
-        bossSprite = new Sprite(fighter);
+        sprite = new Sprite(fighter);
+        sprite.setPosition(300, 900);
         //sprite.setScale(.5f, .5f);
     }
 
 
     public void update(float delta) {
-        //(bossSprite.getOriginY() > )
-        //move to the top of the screen
-        velocity.add(0, -91);
-        velocity.scl(delta);
-        // once it gets to a certain point
-        // move back and forth
-        // fire in patterns
+        Gdx.app.log("spriteOriginY: ", Float.toString(sprite.getX()));
+        if (sprite.getY() > Gdx.graphics.getHeight() / 2) {
+            //move to the top of the screen
+            velocity.add(0, -91);
+        } else {
+            if (!moveRight) {
+                velocity.add(-50, 0);
+                if (sprite.getX() <= 0) {
+                    moveRight = true;
+                }
 
+            } else {
+                velocity.add(50, 0);
+                if (sprite.getX() >= Gdx.graphics.getWidth() - sprite.getWidth()) {
+                    moveRight = false;
+                }
+            }
+
+        }
+        velocity.scl(delta);
+        sprite.translate(velocity.x, velocity.y);
+
+        for (Bullet bullet : world.bullets) {
+            if (sprite.getBoundingRectangle().overlaps(bullet.laserSprite.getBoundingRectangle())) {
+                health--;
+                bullet.kill();
+                if (health <= 0) {
+                    die();
+                    Gdx.app.log("DeceptaBoss", "killed");
+                }
+
+            }
+
+        }
     }
 
     @Override
--- core/src/com/blasters/game/sprites/FighterGenerator.java
+++ core/src/com/blasters/game/sprites/FighterGenerator.java
@@ -10,19 +10,31 @@ import java.util.Random;
  */
 
 public class FighterGenerator {
-
+    private boolean generatedDecepta;
 
     public FighterGenerator(){
-
+        generatedDecepta = false;
     }
 
     public Fighter generate(int level, int spawnValue, GameWorld game) {
+        if (game.hud.getScore() >= 100 && !generatedDecepta) {
+            generatedDecepta = true;
+            return new DeceptaBoss(game);
+
+        }
         Random random = new Random();
         if (level - spawnValue >= 5 && random.nextInt() % 2 == 0) {
-            return new bansheeShip(game);
-        } else {
+           return new bansheeShip(game);
+        }
+        else if(level - spawnValue >= 7 && random.nextInt() % 2 == 0){
+            return new DordraxShip(game);
+        }
+        else{
             return new morcegoShip(game);
         }
+
+
+
     }
 
 }
